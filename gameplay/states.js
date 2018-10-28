9f513cb9b875ad3b0599024d2f98569b24d11598
1024sparrow <1024sparrow@gmail.com>
2018-10-29 00:35:02 +0300

    123

--- gameplay/states.js
+++ gameplay/states.js
@@ -4,47 +4,49 @@ $initState:{
         $bnModeMusic: false
     },
     $booksOrMusic: 'books',
+    // $books: $Traliva.$initData.books - должна быть предусмотрена возможность передавать инициализирующие данные, которые должны формироваться "на-лету", соответственно, они не могут входить в результат компиляции, и присваиваются уже "вдогонку". В этих данных нельзя использовать символ '$'. Для удобства "вклейки" инициализирующих данных результирующий gameplay.js делится на две части - ДО объекта с инициализирующими данными (1.js) и ПОСЛЕ (2.js). Во время выдачи контента пользователю, скрипт должен быть сшит в единый gameplay.js из 1.js, 2.js и инициализирующего объекта.
     $books:{
-        $current:{
-            $book: undefined, // не id, а порядковый номер в списке
-            $fragment: undefined,
-            $block: undefined
+        current:{
+            book: undefined, // не id, а порядковый номер в списке
+            fragment: undefined,
+            block: undefined
         },
-        $list:[
+        list:[
             {
-                $id: 123, // для формирования URL на загрузку
-                $author: 'АСПушкин',
-                $title: 'Руслан и Людмила',
-                $fragmCount: 3
+                id: 123, // для формирования URL на загрузку
+                author: 'АСПушкин',
+                title: 'Руслан и Людмила',
+                fragmCount: 3
             },
             {
-                $id: 124, // для формирования URL на загрузку
-                $author: 'Борис Васильев',
-                $title: 'А зори здесь тихие',
-                $fragmCount: 4
+                id: 124, // для формирования URL на загрузку
+                author: 'Борис Васильев',
+                title: 'А зори здесь тихие',
+                fragmCount: 4
             },
         ]
     },
+    // $music: $Traliva.$initData.music
     $music:{
-        $current: undefined,
-        $list:[
+        current: undefined,
+        list:[
             {
-                $url: 'test_data/music/alb_1',
-                $title: 'album 1'
+                url: 'test_data/music/alb_1',
+                title: 'album 1'
             },
             {
-                $url: 'test_data/music/alb_2',
-                $title: 'album 2'
+                url: 'test_data/music/alb_2',
+                title: 'album 2'
             },
             {
-                $url: 'test_data/music/alb_3',
-                $title: 'album 3'
+                url: 'test_data/music/alb_3',
+                title: 'album 3'
             },
         ],
-        $player:{ // $index формируется исходя из URL, $url уже подставляется логикой
-            $index: undefined,
-            $url: '',
-            $pos: undefined
+        player:{ // $index формируется исходя из URL, $url уже подставляется логикой
+            index: undefined,
+            url: '',
+            pos: undefined
         }
     }
 },
